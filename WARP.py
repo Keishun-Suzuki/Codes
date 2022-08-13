709284cc3951287a0c4fadbe997293a6e1cfd218
Keishun-Suzuki <111195905+Keishun-Suzuki@users.noreply.github.com>
2022-08-14 02:32:19 +0900

    Add files via upload

---
+++ WARP.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+#date A
+pa = np.array([2, 4]) #price vector: (pA1, pA2)
+xa = np.array([2, 6]) #consumption plan: (xA1, xA2)
+eaa = np.dot(pa, xa) #Expenditure (=Budget at date A)
+
+#date B
+pb = np.array([6, 2]) #price vector: (pB1, pB2)
+xb = np.array([6, 4]) #consumption plan: (xB1, xB2)
+ebb = np.dot(pb, xb) #Expenditure (=Budget at date B)
+
+#Counterfactual expenditure
+eab = np.dot(pa, xb) #Expenditure of xB on date A
+eba = np.dot(pb, xa) #Expenditure of xA on date B
+
+if eaa >= eab: #xB is feasible on date A
+    if eba <= ebb: #xA is feasible on date B
+        print('WARP is violated.')
+    else:
+        print('WARP is satisfied.')
+        
+else:
+    if eba <= ebb: #xA is feasible on date B
+        if eab <= eaa: #xB is feasible on date A
+            print('WARP is violated.')
+        else:
+            print('WARP is satisfied.')
+
+import matplotlib.pyplot as plt
+from pylab import mpl
+
+plt.figure(figsize=(10,6))
+plt.axis([0,xa[0]+xb[0],0,xa[1]+xb[1]])
+
+import sympy
+sympy.var('x, y')
+x= np.linspace((xa[0]+xb[0])/100, xa[0]+xb[0]-1/100, 100) 
+
+#Budget lines
+y = -(pa[0]/pa[1])*(x-xa[0])+xa[1] #date A
+plt.plot(x, y, color="crimson", label='Budget line on date A')
+y = -(pb[0]/pb[1])*(x-xb[0])+xb[1] #date B
+plt.plot(x, y, color="darkblue", label='Budget line on date B')
+
+#Points
+plt.plot(xa[0],xa[1], marker='.', color="black", markersize="16")
+plt.plot(xb[0],xb[1], marker='.', color="black", markersize="16")
+
+#Label
+pyplot.text(xa[0]*0.98, xa[1]*1.05, "xA",  fontsize="20")
+pyplot.text(xb[0]*0.98, xb[1]*1.05, "xB",  fontsize="20")
+
+plt.title('Weak Axiom of Revealed Preference',  fontsize="20") 
+plt.xlabel('x1', fontsize="16") 
+plt.ylabel('x2', fontsize="16") 
+
+plt.grid(True)
+plt.legend(fontsize="16") 
+plt.show()
---
+++ pure_exchange_economy.py
@@ -0,0 +1,76 @@
+#This is a Python code for a simple pure exchange economy
+import sympy
+
+#Parameters of Cobb-Douglas utility function
+a = np.array([2, 1]) #consumer A: uA=(x1^a1)*(x2^a2)
+b = np.array([1, 2]) #consumer B: uB=(x1^b1)*(x2^b2)
+
+#Endowment vector
+ea = np.array([3, 4]) #consumer A: (eA1, eA2)
+eb = np.array([6, 2]) #consumer B: (eB1, eB2)
+
+#Initial utility
+uae=ea[0]**a[0]*ea[1]**a[1]
+ube=eb[0]**b[0]*eb[1]**b[1]
+
+#Competitive equilibrium price
+sympy.var('p, pstar')
+pstar=sympy.solve ((ea[0]*p+ea[1])/(1+a[1]/a[0])+(eb[0]*p+eb[1])/(1+b[1]/b[0])-(ea[0]+eb[0])*p, p)
+
+#Competitive equilibrium allocation
+xa1=(ea[0]+ea[1]/pstar[0])/(1+a[1]/a[0])
+xa2=(ea[0]*pstar[0]+ea[1])/(1+a[0]/a[1])
+xb1=(eb[0]+eb[1]/pstar[0])/(1+b[1]/b[0])
+xb2=(eb[0]*pstar[0]+eb[1])/(1+b[0]/b[1])
+
+#Utility
+ua=xa1**a[0]*xa2**a[1]
+ub=xb1**b[0]*xb2**b[1]
+display(pstar[0])
+display([xa1, xa2])
+display([xb1, xb2])
+
+import matplotlib.pyplot as plt
+import numpy as np
+from pylab import mpl
+
+plt.figure(figsize=(12,8))
+plt.axis([0,ea[0]+eb[0],0,ea[1]+eb[1]])
+sympy.var('x, y')
+x= np.linspace((ea[0]+eb[0])/100, ea[0]+eb[0]-1/100, 100)
+
+#Budget line
+y = -pstar[0]*(x-ea[0])+ea[1]
+plt.plot(x, y, color="green", label='Budget line')
+
+#Indifferenct curve
+y = (uae/(x**a[0]))**(1/a[1]) #consumer A (Initial)
+plt.plot(x, y, linestyle="dotted", color="crimson")
+y = ea[1]+eb[1]-(ube/((ea[0]+eb[0]-x)**b[0]))**(1/b[1]) #consumer B (Initial)
+plt.plot(x, y, linestyle="dotted", color="darkblue")
+y = (ua/(x**a[0]))**(1/a[1]) #consumer A
+plt.plot(x, y, color="crimson")
+y = ea[1]+eb[1]-(ub/((ea[0]+eb[0]-x)**b[0]))**(1/b[1]) #consumer B
+plt.plot(x, y, color="darkblue")
+
+#Contract curve
+H = a[1]*b[0]*(ea[1]+eb[1])
+J = a[0]*b[1]*(ea[0]+eb[0])
+K = b[0]*a[1]-a[0]*b[1]
+y= H*x/(J+K*x)
+plt.plot(x, y, linestyle="dashed", color="magenta", label='Contract curve')
+
+#Point
+plt.plot(ea[0],ea[1], marker='.', color="black", markersize="16")
+plt.plot(xa1,xa2, marker='.', color="red", markersize="16")
+
+#Label
+pyplot.text(ea[0]*0.98, ea[1]*1.05, "\u03c9", fontsize="24")
+pyplot.text(xa1*0.98, xa2*1.05, "E", fontsize="24")
+plt.title('Edgeworth Box', fontsize="24")
+plt.xlabel('xA1', fontsize="16")
+plt.ylabel('xA2', fontsize="16")
+
+plt.grid(True)
+plt.legend(fontsize="16")
+plt.show()
